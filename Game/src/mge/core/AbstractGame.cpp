110a8eeb7dc0d9f3cbe3810138f45cbc455fb44c
Tim Akermann <ti.akermann@gmail.com>
2019-02-13 08:48:51 +0100

    Added 8x MSAAntialiasing

--- Game/src/mge/core/AbstractGame.cpp
+++ Game/src/mge/core/AbstractGame.cpp
@@ -38,7 +38,9 @@ void AbstractGame::initialize() {
 
 void AbstractGame::_initializeWindow() {
 	std::cout << "Initializing window..." << std::endl;
-	_window = new sf::RenderWindow( sf::VideoMode(800,600), "My Game!", sf::Style::Default, sf::ContextSettings(24,8,0,3,3));
+	sf::ContextSettings cs = sf::ContextSettings(24, 8, 8, 3, 3);
+	_window = new sf::RenderWindow( sf::VideoMode(800,600), "My Game!", sf::Style::Default, cs);
+	glEnable(GL_MULTISAMPLE);
 	//_window->setVerticalSyncEnabled(true);
     std::cout << "Window initialized." << std::endl << std::endl;
 }
