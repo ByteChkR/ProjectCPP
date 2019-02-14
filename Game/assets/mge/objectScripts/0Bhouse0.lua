b6cfd86e2f84bb033e3e7562cbd899732a5f6f5a
Tim Akermann <ti.akermann@gmail.com>
2019-02-14 09:29:00 +0100

    Added Finns fence. we figured out how 2 export stuff from maya

--- Game/assets/mge/objectScripts/0Bhouse0.lua
+++ Game/assets/mge/objectScripts/0Bhouse0.lua
@@ -1,5 +1,5 @@
 local scripts = {}
-local meta = {["name"] = "0Bhouse0", ["objPath"] = "mge/models/cube_smooth.obj", ["specular"]  = "sp_runicfloor.png", ["texture"] = "runicfloor.png", ["emissive"] = "em_runicfloor.png", ["posX"] = 0, ["posY"] = 0, ["posZ"] = 0 }
+local meta = {["name"] = "0Bhouse0", ["objPath"] = "mge/models/fence0.obj", ["specular"]  = "sp_runicfloor.png", ["texture"] = "wood.jpg", ["emissive"] = "em_runicfloor.png", ["posX"] = 0, ["posY"] = 0, ["posZ"] = 0 }
 collider = { ["width"] = 1, ["height"] = 1, ["depth"] = 1}
 function AttachedScripts()
 	return scripts
