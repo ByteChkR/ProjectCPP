fe5f4c9f9b78d1d49a4ea5fc686f54d5a1623641
Tim Akermann <ti.akermann@gmail.com>
2019-02-16 18:13:28 +0100

    no message

--- Game/src/mge/MGEDemo.cpp
+++ Game/src/mge/MGEDemo.cpp
@@ -113,7 +113,7 @@ void MGEDemo::_initializeScene()
 	_world->setMainCamera(camera);
 
 	//add the floor
-	GameObject* plane = new GameObject("plane", glm::vec3(0, -1, -75));
+	GameObject* plane = new GameObject("plane", glm::vec3(-65, -1, 0));
 	//plane->addBehaviour(new StaticBoxCollider(1, 0, 1));
 	plane->scale(glm::vec3(150, 150, 150));
 	plane->setMesh(planeMeshDefault);
