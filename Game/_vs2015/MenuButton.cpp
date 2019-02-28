5ba1aeffcc60e633252591b2ca346a334abad66c
Tim Akermann <ti.akermann@gmail.com>
2019-02-28 07:15:30 +0100

    Map Chaning Fixes

--- Game/_vs2015/MenuButton.cpp
+++ Game/_vs2015/MenuButton.cpp
@@ -3,6 +3,7 @@
 #include "MenuButton.h"
 #include <iostream>
 #include "../_vs2015/GameStateManager.h"
+#include "LevelManager.h"
 
 MenuButton::MenuButton(sf::RenderWindow *aWindow, sf::Sprite _buttonSprite) : Button(aWindow, _buttonSprite)
 {
@@ -13,5 +14,6 @@ void MenuButton::OnClick()
 {
 	std::cout << "Menu" << std::endl;
 	MapBuilder::instance->Reload();
+	LevelManager::instance->ChangeLevel(0);
 	GameStateManager::instance->_state = GameStateManager::StateMenu;
 }
