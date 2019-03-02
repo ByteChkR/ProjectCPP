799f7347a37748fa08c71dc8e243460a1c735c66
MihaiAdrianC <c_mihaiadrian@yahoo.com>
2019-03-02 18:25:48 +0100

    fixed mesh naming and made first level

--- Game/_vs2015/release_dynamic_exe/mge/objectScripts/0BSideProp5.lua
+++ Game/_vs2015/release_dynamic_exe/mge/objectScripts/0BSideProp5.lua
@@ -1,11 +1,11 @@
 local scripts = {}
 local meta = {["name"] = "0BSideProp5",
-["objPath"] = "mge/models/FarmForestBiome/fence.obj",
+["objPath"] = "mge/models/FarmForestBiome/fence2.obj",
 ["specular"] = "Game/Maps/FarmForestBiome/fence_fence0Shadow1_initialShadingGroup_Specular.png",
 ["texture"] = "Game/Maps/FarmForestBiome/fence_fence0Shadow1_initialShadingGroup_Diffuse.png",
 ["emissive"] = "Game/Maps/FarmForestBiome/fence_fence0Shadow1_initialShadingGroup_Emissive.png",
 ["posX"] = 0,
-["posY"] = 0.1,
+["posY"] = -0.6,
 ["posZ"] = 0  }
 colliderMin = { ["width"] = -1, ["height"] = -1, ["depth"] = -1}
 colliderMax = { ["width"] = 1, ["height"] = 1, ["depth"] = 1}
