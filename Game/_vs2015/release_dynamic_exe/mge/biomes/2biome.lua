358e1bad196b32c611e8488f61be5abe241248de
Tim Akermann <ti.akermann@gmail.com>
2019-03-04 12:46:02 +0100

    Added Corn in the air(offset Y = +5) its ID 36 and in Part Map1_10. Its also listed in the asset naming convention chart

--- Game/_vs2015/release_dynamic_exe/mge/biomes/2biome.lua
+++ Game/_vs2015/release_dynamic_exe/mge/biomes/2biome.lua
@@ -2,4 +2,4 @@ objects = {"none", "2BHouse1","2BHouse2","2BHouse3","2BHouse4","2BHouse5","2BHou
 "2BSideProp1","2BSideProp2","2BSideProp3","2BSideProp4","2BSideProp5","2BSideProp6","2BSideProp7","2BSideProp8",
 "2BLowObstacle1", "2BLowObstacle2", "2BLowObstacle3", "2BLowObstacle4", "2BLowObstacle5",
 "2BHighObstacle1", "2BHighObstacle2", "2BHighObstacle3", "2BHighObstacle4", "2BHighObstacle5", "0BSideProp9", "0BSideProp10",
- "coin", "endoflevel", "turkeycage"}
+ "coin", "endoflevel", "turkeycage", "coinAir"}
