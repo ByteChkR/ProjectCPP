92e1cd3d826033848e595a3d79e883cd48650670
Tim Akermann <ti.akermann@gmail.com>
2019-03-04 19:18:34 +0100

    no message

--- Game/assets/mge/objectScripts/2BLowObstacle1.lua
+++ Game/assets/mge/objectScripts/2BLowObstacle1.lua
@@ -1,10 +1,10 @@
 local scripts = {}
-local meta = {["name"] = "2BLowObstacle1", 
-["objPath"] = "mge/models/CityBiome/17_CityB_Menu_Sign_v1.obj", 
-["specular"] = "Game/Maps/CityBiome/Menu_Sign/17_CityB_Menusign_v1_Menu_Sign_Metallic.png", 
-["texture"] = "Game/Maps/CityBiome/Menu_Sign/17_CityB_Menusign_v1_Menu_Sign_BaseColor.png", 
+local meta = {["name"] = "2BLowObstacle1",
+["objPath"] = "mge/models/CityBiome/17_CityB_Menu_Sign_v1.obj",
+["specular"] = "Game/Maps/CityBiome/Menu_Sign/17_CityB_Menusign_v1_Menu_Sign_Metallic.png",
+["texture"] = "Game/Maps/CityBiome/Menu_Sign/17_CityB_Menusign_v1_Menu_Sign_BaseColor.png",
 ["posX"] = 0,
-["posY"] = 0, 
+["posY"] = 0,
 ["posZ"] = 0 }
 colliderMin = { ["width"] = -1, ["height"] = -1, ["depth"] = -1}
 colliderMax = { ["width"] = 1, ["height"] = 1, ["depth"] = 1}
