6ac5165094d0819d216b6f14ffde4f6c83cd3aab
MihaiAdrianC <c_mihaiadrian@yahoo.com>
2019-03-08 20:02:57 +0100

    no message

--- Game/_vs2015/release_dynamic_exe/mge/maps/map3.lua
+++ Game/_vs2015/release_dynamic_exe/mge/maps/map3.lua
@@ -1,5 +1,5 @@
 heightMapSamplingWidth = "8"
-map = "map2.txt"
+map = "map3.txt"
 ground = "backg3.png"
 mapGround = "ground2.png"
 mapGroundNormal = ""
@@ -11,3 +11,4 @@ heightMapSpeed = "0"
 heightMapMaxHeight = "5"
 xMoveTiling = "100"
 fogColor = {["Red"] = "0.5" , ["Green"] = "0.5" , ["Blue"] = "0.5"}
+lightParameter = "map1Light.lua"
