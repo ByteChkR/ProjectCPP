a3860f227f99ff23fb60c7d972b2f62d6da8a993
LastInfantrie <32453888+LastInfantrie@users.noreply.github.com>
2019-03-08 18:05:45 +0100

    Score Pops up now when it increases

--- Game/src/mge/util/DebugHud.hpp
+++ Game/src/mge/util/DebugHud.hpp
@@ -6,6 +6,7 @@
 #include "../_vs2015/HudText.h"
 #include"../_vs2015/Menu.h"
 #include <iostream>
+#include "../_vs2015/Timer.h"
 
 /**
  * Very simple class capable of rendering a simple piece of text through SFML.
@@ -23,8 +24,11 @@ class DebugHud
 		void Update();
 	private:
 		sf::RenderWindow * _window;
-
+		void OnTick(float pTime);
+		void onEnd();
 		int _score = 0;
+
+		Timer *_timer;
 
 		HudText *_scoreText;
         HudText *_debugText;
