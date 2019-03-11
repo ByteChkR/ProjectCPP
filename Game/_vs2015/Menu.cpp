a152f33b7d20b718a77d136f5bf0a0e1fcd689c3
Tim Akermann <ti.akermann@gmail.com>
2019-03-11 18:24:54 +0100

    Hacked the menu screen to still work while not fullscreen and fullhd

--- Game/_vs2015/Menu.cpp
+++ Game/_vs2015/Menu.cpp
@@ -17,8 +17,8 @@ Menu::Menu(sf::RenderWindow *aWindow) : _window(aWindow)
 
 	_background = new HudSprite("turkey_mainmenu.png");
 
-	_playBox = new HudSprite("play_button.png",1.05f);
-	_button = new PlayButton(_window,_playBox->sprite);
+	_playBox = new HudSprite("play_button.png", 1.05f);
+	_button = new PlayButton(_window, _playBox->sprite);
 
 	_creditsBox = new HudSprite("credits_button.png", 1.05f);
 	_creditsButton = new CreditsButton(_window, _creditsBox->sprite);
@@ -26,34 +26,36 @@ Menu::Menu(sf::RenderWindow *aWindow) : _window(aWindow)
 	_scoreBoardBox = new HudSprite("scoreboard_button.png", 1.05f);
 	_scoreBoardButton = new ScoreBoardButton(_window, _scoreBoardBox->sprite);
 
-	_exitBox = new HudSprite("exit_button.png",1.05f);
+	_exitBox = new HudSprite("exit_button.png", 1.05f);
 	_exitButton = new ExitButton(_window, _exitBox->sprite);
 
 	_organizeMenu();
 }
 
-void Menu::_organizeMenu() 
+void Menu::_organizeMenu()
 {
+	float xoffset = _window->getSize().x < 1920 ? 420 : 1500; //Sheeeeesh 420
+
 	_background->sprite.setPosition(_background->sprite.getTexture()->getSize().x / 2, _background->sprite.getTexture()->getSize().y / 2);
 
-	_playBox->sprite.setPosition(1500, 200);
+	_playBox->sprite.setPosition(xoffset, 200);
 	_playBox->scaledSprite.setPosition(_playBox->sprite.getPosition());
 	_button->SetPosition(_playBox->sprite.getPosition());
 
-	_scoreBoardBox->sprite.setPosition(1500, 350);
+	_scoreBoardBox->sprite.setPosition(xoffset, 350);
 	_scoreBoardBox->scaledSprite.setPosition(_scoreBoardBox->sprite.getPosition());
 	_scoreBoardButton->SetPosition(_scoreBoardBox->sprite.getPosition());
 
-	_creditsBox->sprite.setPosition(1500, 475);
+	_creditsBox->sprite.setPosition(xoffset, 475);
 	_creditsBox->scaledSprite.setPosition(_creditsBox->sprite.getPosition());
 	_creditsButton->SetPosition(_creditsBox->sprite.getPosition());
 
-	_exitBox->sprite.setPosition(1500, 600);
+	_exitBox->sprite.setPosition(xoffset, 600);
 	_exitBox->scaledSprite.setPosition(_exitBox->sprite.getPosition());
 	_exitButton->SetPosition(_exitBox->sprite.getPosition());
 }
 
-void Menu::Update() 
+void Menu::Update()
 {
 	_button->Update();
 	_exitButton->Update();
@@ -62,7 +64,7 @@ void Menu::Update()
 	draw();
 }
 
-void Menu::draw() 
+void Menu::draw()
 {
 	glActiveTexture(GL_TEXTURE0);
 	_window->pushGLStates();
@@ -78,7 +80,7 @@ void Menu::draw()
 	if (_scoreBoardButton->scaled)_window->draw(_scoreBoardBox->scaledSprite);
 	else _window->draw(_scoreBoardBox->sprite);
 
-	if(_exitButton->scaled)_window->draw(_exitBox->scaledSprite);
+	if (_exitButton->scaled)_window->draw(_exitBox->scaledSprite);
 	else _window->draw(_exitBox->sprite);
 
 	_window->popGLStates();
