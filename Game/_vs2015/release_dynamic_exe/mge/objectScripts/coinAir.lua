86178b4f0a439bb4e26886c231dac1542b2c95bf
Tim Akermann <ti.akermann@gmail.com>
2019-03-12 08:35:29 +0100

    Removed the C# Code from the project and moved it to map editor solution/repo

--- Game/_vs2015/release_dynamic_exe/mge/objectScripts/coinAir.lua
+++ Game/_vs2015/release_dynamic_exe/mge/objectScripts/coinAir.lua
@@ -1,7 +1,7 @@
 local scripts = {}
 local meta = {["name"] = "coinAir", 
 ["objPath"] = "mge/models/Corn_pickup_baloon.obj", 
-["specular"] = "Game/Shared/Corn/Baloon/Corn_pickup_baloon_Baloon_Corn_Roughness.png", 
+["specular"] = "white.png", 
 ["texture"] = "Game/Shared/Corn/Baloon/Corn_pickup_baloon_Baloon_Corn_BaseColor.png", 
 ["emissive"] = "Game/Shared/Corn/Baloon/Corn_pickup_baloon_Baloon_Corn_Emissive.png",
 ["posX"] = 0, 
@@ -10,6 +10,7 @@ local meta = {["name"] = "coinAir",
 colliderMin = { ["width"] = -1, ["height"] = -1, ["depth"] = -1}
 colliderMax = { ["width"] = 1, ["height"] = 1, ["depth"] = 1}
 colliderMode = "auto"
+Shininess = 20
 function AttachedScripts()
 	return scripts
 end
