bf283a4456131f8b106329eb1c1a42366d705062
Tim Akermann <ti.akermann@gmail.com>
2019-03-12 22:25:40 +0100

    no message

--- Game/_vs2015/release_dynamic_exe/mge/objectScripts/1BSideProp8.lua
+++ Game/_vs2015/release_dynamic_exe/mge/objectScripts/1BSideProp8.lua
@@ -3,7 +3,7 @@ local scripts = {}
 local meta = {["name"] = "1BSideProp8", 
 ["objPath"] = "mge/models/VillageForestBiome/16_VillageB_PineTree2_V1.obj",
 ["specular"] = "black.png",
-["texture"] = "Game/Maps/VillageForestBiome/randombonsai/tex.jpg",
+["texture"] = "Game/Maps/VillageForestBiome/pinetree/2/14_VillageB_PineTree3_V2_Geom_AlbedoTransparency.png",
 --["emissive"] = "Game/Maps/VillageForestBiome/pinetree/3/14_VillageB_PineTree3_V3_Geom_AlbedoTransparency.png",
 ["posX"] = 0, ["posY"] = -1, ["posZ"] = 0}
 colliderMin = { ["width"] = -1, ["height"] = -1, ["depth"] = -1}
