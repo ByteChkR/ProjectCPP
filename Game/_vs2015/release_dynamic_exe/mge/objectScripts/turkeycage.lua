cbee80d4f728d2d1cbcee0de196d362775a65131
Tim Akermann <ti.akermann@gmail.com>
2019-03-14 10:29:16 +0100

    Turkey cage raised by 0.2

--- Game/_vs2015/release_dynamic_exe/mge/objectScripts/turkeycage.lua
+++ Game/_vs2015/release_dynamic_exe/mge/objectScripts/turkeycage.lua
@@ -5,7 +5,7 @@ local meta = {["name"] = "turkeycage",
 ["specular"] = "black.png",
 ["emissive"] = "black.png",
 ["posX"] = 0, 
-["posY"] = -1, 
+["posY"] = -0.8, 
 ["posZ"] = 0 }
 colliderMin = { ["width"] = -1, ["height"] = -1, ["depth"] = -1}
 colliderMax = { ["width"] = 1, ["height"] = 1, ["depth"] = 1}
